8e253a05760ca49fe4388efd40bf2948eb8b6798
bodeludvig <bodeludvig@gmail.com>
2025-02-17 11:59:29 +0100

    Made a category selection for selecting products by category, updated from master
    Signed-off-by: bodeludvig <bodeludvig@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,8 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        CustomerController customerController = new CustomerController();
-        customerController.run();
+        /*CustomerController customerController = new CustomerController();
+        customerController.run();*/
 
         ProductController productController = new ProductController();
         productController.run();
